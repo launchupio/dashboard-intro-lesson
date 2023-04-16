f17d52f1e77674b7816defad3db9b2e9901c2489
neli-rashkova <neli.rashkova73@gmail.com>
2023-04-16 13:29:17 -0400

    Modified input styles to change when input is focused. Modified company info component styling. Created MainContent.css file to add necessary styling to input element.

--- src/components/CompanyInfo.js
+++ src/components/CompanyInfo.js
@@ -1,18 +1,30 @@
-
-function CompanyInfo({ icon, iconbg, text, number, stat, stattext }) {
-    return (
-        <div className="h-[120px] border-[2px] border-black">
-            <div className="flex justify-between px-1 h-[60%] border-b-[1px] border-gray-700">
-                <div className={`${iconbg} w-fit rounded`}>{icon}</div>
-                <div className="text-right">
-                    <p className="text-xs text-gray-700">{text}</p>
-                    <p className="font-bold">{number}</p>
-                </div>
-            </div>
-            <div className="">
-                <p className="border-[1px] border-black">{`${stat} ${stattext}`}</p>
-            </div>
+function CompanyInfo({
+  icon,
+  iconbg,
+  text,
+  number,
+  stat,
+  statcolor,
+  stattext,
+}) {
+  return (
+    <div className="h-[150px] px-1 rounded-lg bg-white font-sans">
+      <div className="relative px-1 pt-2 h-[60%] border-b-[0.5px] border-gray-500">
+        <div
+          className={`${iconbg} absolute -top-2 w-[3rem] h-[3.1rem] flex justify-center items-center text-[1.2rem] rounded-lg drop-shadow-lg`}
+        >
+          <span className="text-white text-xl">{icon}</span>
         </div>
-    );
+        <div className="text-right">
+          <p className="text-sm text-gray-500">{text}</p>
+          <p className="font-bold text-xl">{number}</p>
+        </div>
+      </div>
+      <div className="px-1 h-[40%] flex items-center text-sm">
+        <p className={`pr-1 ${statcolor}`}>{stat}</p>
+        <p className="text-gray-500">{stattext}</p>
+      </div>
+    </div>
+  );
 }
 export default CompanyInfo;
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,21 +1,37 @@
-import { 
-  BsFillPersonFill, FiMenu, FcSettings, IoNotificationsSharp, FaCouch, BsPersonFill
-} from '../utils/icons';
-import CompanyInfo from './CompanyInfo';
+import {
+  BsFillPersonFill,
+  FiMenu,
+  FcSettings,
+  IoNotificationsSharp,
+  FaCouch,
+  BsPersonFill,
+} from "../utils/icons";
+import CompanyInfo from "./CompanyInfo";
+import "./MainContent.css";
 
 export default function MainContent() {
   return (
-    <div className="w-screen h-screen grid grid-cols-2 border-[2px] border-white overflow-y-auto">
-      <div className="w-[calc(100vw-238px)] h-full ml-[218px] grid grid-cols-6 grid-rows-12 border-[2px] border-black">
+    <div className="box w-screen h-screen grid grid-cols-2 overflow-y-auto bg-gray-200">
+      <div className="w-[calc(100vw-238px)] h-full ml-[218px] grid grid-cols-6 auto-rows-min gap-5">
         {/* Nav section */}
-        <div className="col-start-1 col-end-7 row-start-1 row-end-2 flex justify-between border-[2px] border-blue-300 text-sm">
+        <div className="col-start-1 col-end-7 row-start-1 row-end-2 flex justify-between text-sm mb-2 mt-2 p-2">
           <div className="flex flex-col">
             <div>Pages / Dashboard</div>
             <div className="font-semibold">Dashboard</div>
           </div>
-          <div className="flex items-center justify-evenly w-[430px]">
-            <input className="border-[1px] py-[2px] px-[2px] rounded-md border-gray-300" />
-            <button className="border-[1px] rounded-md border-pink-400 text-xs py-[5px] px-[5px] w-[100px] text-pink-400">Online Builder</button>
+          <div className="flex items-center justify-evenly w-[500px]">
+            <label className="w-fit relative">
+              <input
+                className="input border-[1px] w-[175px] h-[35px] py-[2px] px-[8px] rounded-md border-gray-400 bg-transparent focus:outline-none focus:border-pink-600 placeholder:opacity-0"
+                placeholder="&nbsp;"
+              />
+              <span className="label absolute text-ellipsis overflow-hidden whitespace-nowrap text-sm top-2 left-2 w-[calc(100%-18px)] text-gray-500">
+                Type here...
+              </span>
+            </label>
+            <button className="border-[1px] h-[30px] rounded-md border-pink-600 text-xs p-1 w-[125px] text-pink-600 font-semibold uppercase transition ease-in-out hover:duration-500  hover:border-pink-400 hover:text-pink-400">
+              Online Builder
+            </button>
             <div className="flex items-center justify-evenly w-[160px]">
               <BsFillPersonFill />
               <div>Sign In</div>
@@ -26,21 +42,61 @@ export default function MainContent() {
           </div>
         </div>
         {/* Company info section */}
-        <div className="col-start-1 col-span-6 row-start-2 row-end-4 grid grid-cols-4 border-[2px] border-black">
-          <CompanyInfo icon={<FaCouch style={{backgroundColor: 'white'}} />} iconbg="bg-slate-700" text={`Today's Money`} number="$53k" stat="+55%" stattext="than last week" />
-          <CompanyInfo icon={<BsPersonFill />} iconbg="bg-pink-400" text={`Today's Users`} number="2,300" stat="+3%" stattext="than last month" />
-          <CompanyInfo icon={<BsPersonFill />} iconbg="bg-green-400" text="New Clients" number="3,462" stat="-2%" stattext="than yesterday" />
-          <CompanyInfo icon={<FaCouch />} iconbg="bg-blue-400" text="Sales" number="$103,430" stat="+5%" stattext="than yesterday" />
+        <div className="col-start-1 col-span-6 row-start-2 row-end-4 grid grid-cols-4 gap-4">
+          <CompanyInfo
+            icon={<FaCouch />}
+            iconbg="bg-slate-700"
+            text={`Today's Money`}
+            number="$53k"
+            stat="+55%"
+            statcolor="text-green-500"
+            stattext="than last week"
+          />
+          <CompanyInfo
+            icon={<BsPersonFill />}
+            iconbg="bg-pink-400"
+            text={`Today's Users`}
+            number="2,300"
+            stat="+3%"
+            statcolor="text-green-500"
+            stattext="than last month"
+          />
+          <CompanyInfo
+            icon={<BsPersonFill />}
+            iconbg="bg-green-400"
+            text="New Clients"
+            number="3,462"
+            stat="-2%"
+            statcolor="text-red-500"
+            stattext="than yesterday"
+          />
+          <CompanyInfo
+            icon={<FaCouch />}
+            iconbg="bg-blue-400"
+            text="Sales"
+            number="$103,430"
+            stat="+5%"
+            statcolor="text-green-500"
+            stattext="than yesterday"
+          />
         </div>
         {/* graphs section */}
-        <div className="col-start-1 col-span-4 row-start-4 row-end-7 border-[2px] border-black">Graphs</div>
+        <div className="col-start-1 col-span-4 row-start-4 row-end-7 border-[2px] border-black">
+          Graphs
+        </div>
         {/* Project section */}
-        <div className="col-start-1 col-span-4 row-start-7 row-end-12 inline-block border-[2px] border-black">Project info</div>
+        <div className="col-start-1 col-span-4 row-start-7 row-end-12 inline-block border-[2px] border-black">
+          Project info
+        </div>
         {/* grpah and orders section */}
-        <div className="col-start-5 col-span-2 row-start-4 row-end-12 border-[1px] border-red-500 bg-green-200">Graph and Order section</div>
+        <div className="col-start-5 col-span-2 row-start-4 row-end-12 border-[1px] border-red-500 bg-green-200">
+          Graph and Order section
+        </div>
         {/* footer section */}
-        <div className="col-start-1 col-span-6 row-start-12 row-end-13 border-[2px] border-black">Footer section</div>
+        <div className="col-start-1 col-span-6 row-start-12 row-end-13 border-[2px] border-black">
+          Footer section
+        </div>
       </div>
     </div>
-  )
-} 
+  );
+}
